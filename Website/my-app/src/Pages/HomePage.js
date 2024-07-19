fa15a5299d6103d592ec4fa45b6cc3339f1479e1
Tr3ntroxs <113727648+Tr3ntroxs@users.noreply.github.com>
2024-07-19 14:47:33 -0400

    Contact section complete
    - Updated transitions for buttons/logos
    - Formatting and font restored
    - It actually works

--- Website/my-app/src/Pages/HomePage.js
+++ Website/my-app/src/Pages/HomePage.js
@@ -1,8 +1,42 @@
-import "./HomePage.css"
+import "./HomePage.css";
+
+// Services
+import { send } from "emailjs-com";
+
+// Hooks
+import React, { useState } from 'react';
+
+// Assets
 import { FaPaperPlane, FaYoutube, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { ImPhone } from "react-icons/im";
 
 export default function HomePage() {
+
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        message: '',
+        reply_to: '',
+    });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'service_01bnghk',
+            'template_hkfut9z',
+            toSend,
+            'Z0tSftjM8zMs8Yv_V'
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+            });
+    };
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
 
     return (
         <div className="page">
@@ -29,17 +63,45 @@ export default function HomePage() {
                             <p><i><FaPaperPlane /></i>testingllamas@gmail.com</p>
                             <p><i><ImPhone /></i>978-223-7494</p>
                             <div className="social">
-                                <a href="https://www.instagram.com/trent.maffeo/?next=%2F"><FaInstagram /></a>
-                                <a href="https://www.youtube.com/channel/UCZmEW-klHYOcHQWprsOgCOw"><FaYoutube /></a>
-                                <a href="https://www.linkedin.com/in/trent-maffeo-270747228/"><FaLinkedinIn /></a>
+                                <a href="https://www.instagram.com/trent.maffeo/?next=%2F" className="insta"><FaInstagram /></a>
+                                <a href="https://www.youtube.com/channel/UCZmEW-klHYOcHQWprsOgCOw" className="yt"><FaYoutube /></a>
+                                <a href="https://www.linkedin.com/in/trent-maffeo-270747228/" className="linkedin"><FaLinkedinIn /></a>
                             </div>
                             <a href="../Images/resume.pdf" download className="btn">Download CV</a>
                         </div>
                         <div className="contact-right">
-                            <form className="submit-to-google-sheet"> 
-                                <input type="text" name="Name" placeholder="Your Name" required></input>
-                                <input type="email" name="email" placeholder="Your Email" required></input>
+                            <form onSubmit={onSubmit}>
+                                
+                                {/* <textarea name="Email" rows="1" placeholder="email (i.e. johnsmith@gmail.com)"></textarea>
+                                <hr></hr>
                                 <textarea name="Message" rows="6" placeholder="Your Message"></textarea>
+                                <button type="submit" className="btn">Submit</button> */}
+                                
+                                <textarea
+                                    type='text'
+                                    name='from_name'
+                                    rows='1'
+                                    placeholder='Your name'
+                                    value={toSend.from_name}
+                                    onChange={handleChange}
+                                />
+                                <textarea
+                                    type='text'
+                                    name='reply_to'
+                                    rows='1'
+                                    placeholder='Your email'
+                                    value={toSend.reply_to}
+                                    onChange={handleChange}
+                                />
+                                <hr></hr>
+                                <textarea
+                                    type='text'
+                                    name='message'
+                                    rows='6'
+                                    placeholder='Tell me all about it!'
+                                    value={toSend.message}
+                                    onChange={handleChange}
+                                />
                                 <button type="submit" className="btn">Submit</button>
                             </form>
                             {/* <span id="msg"></span> */}
